2a72685989e31ef24c72c35d9b9043f9b1389f2b
Lucasjrt <lucasjrt@outlook.com>
2018-10-15 16:05:21 -0300

    v1.8
    Importa cores somente se no sistema UNIX

--- automato.c
+++ automato.c
@@ -1,12 +1,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include "automato.h"
-//#define COLOR_RED     "\x1b[31m"
-#define COLOR_RED ""
-//#define COLOR_GREEN   "\x1b[32m"
-#define COLOR_GREEN ""
-//#define COLOR_RESET   "\x1b[0m"
-#define COLOR_RESET ""
+#ifdef _unix_
+  #define COLOR_RED     "\x1b[31m"
+  #define COLOR_GREEN   "\x1b[32m"
+  #define COLOR_RESET   "\x1b[0m"
+#elif defined(_WIN32) || defined(WIN32)
+  #define COLOR_RED     ""
+  #define COLOR_GREEN   ""
+  #define COLOR_RESET   ""
+#endif
 
 struct delta {
     char estado1[15];
