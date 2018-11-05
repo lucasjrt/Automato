76588cf80fb41806f9725e484461fe30e5502219
Unknown <tarcisioaraujo123@hotmail.com>
2018-11-04 23:48:17 -0200

    v2.2
    Adicionada a funcionalidade de ler expressões regulares

--- main.c
+++ main.c
@@ -9,8 +9,18 @@
 
 int main() {
     char cadeia[200],repete='s';
-    int reconheceu;
-    Automato a = carrega_automato("Automato05.dat");
+    int reconheceu,opcao;
+    Automato a;
+    printf("1 - Ler de arquivo\n2 - Ler expressao regular\n3 - SAIR\nEscolha uma opcao: ");
+    scanf("%d",&opcao);
+    if(opcao==1)a = carrega_automato("Automato05.dat");
+    else if(opcao==2){
+        char sequencia[100];
+        printf("Digite a expressao regular: \n");
+        scanf("%s",sequencia);
+        a = trataSeq(sequencia);
+        //mostrarAutomato(a);
+    }else return 1;
     while(repete=='s'){
         printf("Digite a cadeia para verificar se o automato a reconhece: ");
         scanf("%s",cadeia);
